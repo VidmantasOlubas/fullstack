4b660f5fe54f146610f9ecb7347bc91f605c75a7
VidmantasOlubas <olubasvidmantas123@gmail.com>
2022-05-03 12:00:56 +0300

    1.6

--- unicafe/src/App.js
+++ unicafe/src/App.js
@@ -1,21 +1,38 @@
 import React from 'react';
-import {useState} from 'react'
+import { useState } from 'react'
 
-function App() {
+function App()
+{
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+
+  let handleButtonGoodClick = function ()
+  {
+    setGood(good + 1)
+  }
+
+  let handleButtonNeutralClick = function ()
+  {
+    setNeutral(neutral + 1)
+  }
+
+  let handleButtonBadClick = function ()
+  {
+    setBad(bad + 1)
+  }
+
   return (
-<div>
-  <h1>give feedback</h1>
-  <button>good</button>
-  <button>neutral</button>
-  <button>bad</button>
-  <h1>statistics</h1>
-  <p>good {good}</p>
-  <p>neutral {neutral}</p>
-  <p>bad {bad}</p>
-</div>
+    <div>
+      <h1>give feedback</h1>
+      <button onClick={handleButtonGoodClick}>good</button>
+      <button onClick={handleButtonNeutralClick}>neutral</button>
+      <button onClick={handleButtonBadClick}>bad</button>
+      <h1>statistics</h1>
+      <p>good {good}</p>
+      <p>neutral {neutral}</p>
+      <p>bad {bad}</p>
+    </div>
   )
 }
 
