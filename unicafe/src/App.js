2626d367431a82a4fadc4f936a1fc96d8c049dd5
VidmantasOlubas <olubasvidmantas123@gmail.com>
2022-05-03 11:51:06 +0300

    1.6

---
+++ unicafe/src/App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import {useState} from 'react'
+
+function App() {
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+  return (
+<div>
+  <h1>give feedback</h1>
+  <button>good</button>
+  <button>neutral</button>
+  <button>bad</button>
+  <h1>statistics</h1>
+  <p>good {good}</p>
+  <p>neutral {neutral}</p>
+  <p>bad {bad}</p>
+</div>
+  )
+}
+
+export default App;
