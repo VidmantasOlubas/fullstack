109ab1aa981df886605c6d6019c9c5c5eea141fb
VidmantasOlubas <olubasvidmantas123@gmail.com>
2022-05-23 11:05:34 +0300

    2.8bug

--- phonebook/src/App.js
+++ phonebook/src/App.js
@@ -3,48 +3,65 @@ import { useState, createRef } from 'react'
 const App = () =>
 {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' },
-    { name: 'Vidmantas' },
-    { name: 'aaaaaa' },
-    { name: 'bbbbbb' },
-    { name: 'cccccc' }
+    { name: 'Arto Hellas', number: "1" },
+    { name: 'Vidmantas', number: "2" },
+    { name: 'aaaaaa', number: "3" },
+    { name: 'bbbbbb', number: "4" },
+    { name: 'cccccc', number: "5" }
   ])
   const [newName, setNewName] = useState('')
 
   const refInputName = createRef()
 
-  const fsubmit = () =>{
-    console.log(refInputName.current.value)
-    
+  const refInputNumber = createRef()
 
-      for(let i = 0;i < persons.length; i++){
-        console.log('tikrinu ar ' + refInputName.current.value + ' === ' + persons[i].name )
-        if (refInputName.current.value === persons[i].name){
-          return alert(refInputName.current.value + ' is already here')
-        }
+  const fsubmit = () =>
+  {
+    console.log(refInputName.current.value)
+    console.log(refInputNumber.current.value)
+
+    //paieska masyve persons
+    for (let i = 0; i < persons.length; i++)
+    {
+
+      if (refInputName.current.value === persons[i].name)
+      {
+        alert(refInputName.current.value + ' is already here')
+
+        return
       }
-    setPersons([{name: refInputName.current.value},...persons])
+    }
+
+    const copyOfPersons = [...persons]
+
+    copyOfPersons.push({ name: refInputName.current.value, number: refInputNumber.current.value })
+
+    setPersons(copyOfPersons)
+
   }
+
+
 
   const array = []
 
   for (let i = 0; i < persons.length; i++)
   {
-    array.push(<p key={i}>{persons[i].name}</p>)
+    array.push(<p key={i}>{persons[i].name} {persons[i].number}</p>)
   }
 
   return (
     <div>
       <h2>Phonebook</h2>
 
-
-      <div>
-        name: <input ref={refInputName} />
-      </div>
-      <div>
-        <button onClick={fsubmit}>add</button>
-      </div>
-
+      <form>
+        <div>
+          name: <input ref={refInputName} />
+        </div>
+        <div>number: <input ref={refInputNumber} /></div>
+        <div>
+          <button onClick={fsubmit}>add</button>
+        </div>
+      </form>
 
       <h2>Numbers</h2>
       {array}
--- phonebook/test.js
+++
@@ -1,3 +0,0 @@
-const aaaaaa = [1,2,3,4,5]
-const bbbbbb = aaaaaa.includes(1)
-console.log(bbbbbb)
